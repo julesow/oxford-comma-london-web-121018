6889ba5b87ed20f285402b72d63bf4669996c260
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-11-29 21:34:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  array.join(",")
+  if array.length == 2
+    array.join(" and ")
+  else array.length > 3
+    last_array == array[-1].join(" and ")
+    array.pop
+    array.push(last_array)
+  end
 
 end
+
+# array = ["kiwi", "durian","starfruit", "mangos", "dragon fruits"]
+# puts oxford_comma(array)
